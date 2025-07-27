b7fd88436be5aea828c3067b2211c6bb789d4a49
Mohammed-908 <scriptcode0@gmail.com>
2025-07-27 13:33:47 +0200

    Synaste commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+let HemburgerMenu = document.getElementById("HemburgerMenu");
+let NavForMobile = document.querySelector(".NavForMobile");
+
+HemburgerMenu.addEventListener('click', function(){
+  NavForMobile.style.display = "block";
+})
+
+
+
 fetch('https://api.aladhan.com/v1/timingsByCity?city=Boras&country=Sweden&method=8')
   .then(response => response.json())
   .then(data => {
@@ -9,3 +19,45 @@ fetch('https://api.aladhan.com/v1/timingsByCity?city=Boras&country=Sweden&method
     document.querySelector('.prayer-time.isha').textContent = timings.Isha;
   })
   .catch(err => console.error('Error fetching prayer times:', err));
+
+
+
+      function showLoader() {
+      document.getElementById('loaderModal').classList.add('active');
+    }
+
+    function hideLoader() {
+      document.getElementById('loaderModal').classList.remove('active');
+    }
+
+    // Exempel: visa loader i 2 sekunder när sidan laddas
+    window.addEventListener('load', () => {
+      showLoader();
+      setTimeout(hideLoader, 500);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
