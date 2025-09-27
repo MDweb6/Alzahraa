4e6145aed4493d94e587225c502bd940454f2a90
Mohammed-908 <scriptcode0@gmail.com>
2025-09-27 19:22:18 +0200

    v.2

---
+++ ar/script.js
@@ -0,0 +1,131 @@
+let HemburgerMenu = document.getElementById("HemburgerMenu");
+let CloseMenu = document.getElementById("CloseMenu");
+let NavForMobile = document.querySelector(".NavForMobile");
+
+HemburgerMenu.addEventListener('click', function(){
+  NavForMobile.style.display = "block";
+  setTimeout(() => {
+    NavForMobile.classList.add("open");
+  }, 10);
+});
+
+CloseMenu.addEventListener('click', function(){
+  NavForMobile.classList.remove("open");
+  NavForMobile.addEventListener('transitionend', function handler(){
+    NavForMobile.style.display = "none";
+    NavForMobile.removeEventListener('transitionend', handler);
+  });
+});
+
+let Langu = document.querySelector(".Langu");
+let LanguSide = document.querySelector(".LanguSide");
+
+Langu.addEventListener('click', function() {
+  // Kolla om border-radius redan är satt
+  if (Langu.style.borderRadius === "15px 0px 0px 15px") {
+    // Om ja, återställ till standard
+    Langu.style.borderRadius = ""; // tar bort inline-stilen
+    LanguSide.style.display = "none";
+  } else {
+    Langu.style.borderRadius = "15px 0px 0px 15px";
+    LanguSide.style.display = "flex";
+  }
+});
+
+
+
+fetch('https://api.aladhan.com/v1/timingsByCity?city=Boras&country=Sweden&method=8')
+  .then(response => response.json())
+  .then(data => {
+    const timings = data.data.timings;
+    document.querySelector('.prayer-time.fajr').textContent = timings.Fajr;
+    document.querySelector('.prayer-time.dhuhr').textContent = timings.Dhuhr;
+    document.querySelector('.prayer-time.asr').textContent = timings.Asr;
+    document.querySelector('.prayer-time.maghrib').textContent = timings.Maghrib;
+    document.querySelector('.prayer-time.isha').textContent = timings.Isha;
+  })
+  .catch(err => console.error('Error fetching prayer times:', err));
+
+
+
+      function showLoader() {
+      document.getElementById('loaderModal').classList.add('active');
+    }
+
+    function hideLoader() {
+      document.getElementById('loaderModal').classList.remove('active');
+    }
+
+    window.addEventListener('load', () => {
+      showLoader();
+      setTimeout(hideLoader, 600);
+    });
+
+
+let VisaDua = document.querySelectorAll(".VisaDua");
+
+VisaDua.forEach(function(element) {
+  element.addEventListener("click", function() {
+    // Om du vill navigera till ny sida:
+    window.location.href = "arDua.html";
+  });
+});
+
+
+
+let sida1 = document.querySelectorAll(".sida1");
+sida1.forEach(function(element) {
+  element.addEventListener("click", function() {
+    window.location.href = "ar.html";
+  });
+});
+
+let sida2 = document.querySelectorAll(".sida2");
+
+sida2.forEach(function(element) {
+  element.addEventListener("click", function(e) {
+    e.preventDefault(); // Hindra direkt klick
+    sessionStorage.setItem("scrollTo", "OvSec"); // Spara vilket element vi vill scrolla till
+    window.location.href = "ar.html"; // Skicka till startsidan
+  });
+});
+
+
+let sida3 = document.querySelectorAll(".sida3");
+
+sida3.forEach(function(element) {
+  element.addEventListener("click", function(e) {
+    e.preventDefault(); // Hindra direkt klick
+    sessionStorage.setItem("scrollTo", "slutet"); // Spara vilket element vi vill scrolla till
+    window.location.href = "ar.html"; // Skicka till startsidan
+  });
+});
+
+
+let LogoIndex = document.getElementById("LogoIndex");
+if (LogoIndex) {
+  LogoIndex.addEventListener("click", function() {
+    window.location.href = "ar.html";  // Navigera till ar.html i samma fönster
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
